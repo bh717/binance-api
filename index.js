27d54455c0ea75a248c44a2dfa0044dc29583cf9
Ubuntu <ubuntu@ip-172-31-5-215.ap-southeast-1.compute.internal>
2022-11-27 20:21:10 +0000

    complete

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
-import Binance from 'binance-api-node';
+import Binance from 'node-binance-api';
 
-
-const client = Binance.default({
-  apiKey: 'f9ycVKSEWRuDIuGPbwfgYNxeZIQDh03WQw1JvSLmNgymaZLSdnx1LfT2knXNWTdO',
-  apiSecret: 'mrzCiLQEcXEGixmIaa3dP5yV8ww9vC19rfIpzj5cuE928xoF0qoLc5uAVy6qXncN',
+const binance = new Binance().options({
+  APIKEY: 'f9ycVKSEWRuDIuGPbwfgYNxeZIQDh03WQw1JvSLmNgymaZLSdnx1LfT2knXNWTdO',
+  APISECRET: 'mrzCiLQEcXEGixmIaa3dP5yV8ww9vC19rfIpzj5cuE928xoF0qoLc5uAVy6qXncN'
 });
 
+//Get balance of future wallet
+const futurearray = await binance.futuresBalance();
+let futurebalance = 0;
+let ticker = await binance.prices();
 
-console.log(await client.futuresPing())
+futurearray.map((futurebal) => {
+  let curren = String(futurebal.asset);
+  if (curren === "DOT") {
+    futurebalance = futurebalance + (Number(ticker.DOTUSDT) * Number(futurebal.balance));
+  }
+  else if (curren === "BTC") {
+    futurebalance = futurebalance + (Number(ticker.BTCUSDT) * Number(futurebal.balance));
+  }
+  else if (curren === "SOL") {
+    futurebalance = futurebalance + (Number(ticker.SOLUSDT) * Number(futurebal.balance));
+  }
+  else if (curren === "BNB") {
+    futurebalance = futurebalance + (Number(ticker.BNBUSDT) * Number(futurebal.balance));
+  }
+  else if (curren === "ETH") {
+    futurebalance = futurebalance + (Number(ticker.ETHUSDT) * Number(futurebal.balance));
+  }
+  else if (curren === "ADA") {
+    futurebalance = futurebalance + (Number(ticker.ADAUSDT) * Number(futurebal.balance));
+  }
+  else if (curren === "XRP") {
+    futurebalance = futurebalance + (Number(ticker.XRPUSDT) * Number(futurebal.balance));
+  }
+  else {
+    futurebalance = futurebalance + Number(futurebal.balance);
+  }
+})
 
-console.log(await client.futuresTime())
+console.log(futurebalance);
 
-console.log(await client.futuresAccountBalance());
+//Get balance of spot wallet
+const spotBalance = await binance.balance()
 
-// const binance = new Binance({
-//   APIKEY: 'f9ycVKSEWRuDIuGPbwfgYNxeZIQDh03WQw1JvSLmNgymaZLSdnx1LfT2knXNWTdO',
-//   APISECRET: 'mrzCiLQEcXEGixmIaa3dP5yV8ww9vC19rfIpzj5cuE928xoF0qoLc5uAVy6qXncN',
-// }).default;
+let result = Object.keys(spotBalance).map((key) => [
+  String(key),
+  spotBalance[key],
+])
+
+let prices = await binance.prices()
+let prices_array = Object.keys(prices).map((key) => [
+  String(key),
+  prices[key],
+])
 
 
-// client.post('/binancepay/openapi/v2/balance',{
-//   wallet: "SPOT_WALLET",
-//   currency: "BUSD"
-// }).then((response)=>{
-//   console.log(response.data);
-// })
+let coinAmount = 0;
 
+for (let k = 0; k < result.length; k++) {
+  if (result[k][0] === 'USDT') {
+    console.log(result[k][0])
+    const price = Number(result[k][1].available) + Number(result[k][1].onOrder)
+    coinAmount = coinAmount + price;
+  }
+  for (let j = 0; j < prices_array.length; j++) {
+
+    if (prices_array[j][0] === result[k][0] + 'USDT') {
+      const price = (Number(result[k][1].available) * Number(prices_array[j][1])) + (Number(result[k][1].onOrder) * Number(prices_array[j][1]));
+      coinAmount = coinAmount + price;
+    }
+  }
+}
+
+console.log(coinAmount + futurebalance);
